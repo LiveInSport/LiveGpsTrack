a7b374dd26a48e11a0dca473c6b39961bc75390a
Kaido Kurm <kaido.diato@gmail.com>
2018-09-27 16:24:57 +0300

    test

--- LiveGpsTrack/Login/LogIn.aspx
+++ LiveGpsTrack/Login/LogIn.aspx
@@ -1,5 +1,9 @@
 ﻿<%@ Page Title="Log In" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="LogIn.aspx.cs" Inherits="LiveGpsTrack.WebForm2" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
     <asp:HyperLink ID="HyperLink1" NavigateUrl="~/Login/CreateUser.aspx" runat="server">Register User</asp:HyperLink>
-<asp:Login ID="Login1" runat="server"></asp:Login>
+    <asp:Login ID="Login1" runat="server" OnAuthenticate="Login1_Authenticate">
+    </asp:Login>
+    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" SelectCommand="SELECT * FROM [login]"></asp:SqlDataSource>
 </asp:Content>
+
--- LiveGpsTrack/Login/LogIn.aspx.cs
+++ LiveGpsTrack/Login/LogIn.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace LiveGpsTrack
 {
@@ -11,6 +14,23 @@ namespace LiveGpsTrack
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+
+        }
+
+        protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
+        {
+            SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+            sqlConnection.Open();
+            SqlCommand cmd = new SqlCommand("select * from login where uname=@username and passwd=@password", sqlConnection);
+            cmd.Parameters.AddWithValue("@username", TextBox.)
+        }
+
+        protected void LoginButton_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+            sqlConnection.Open();
+            SqlCommand cmd = new SqlCommand("select * from login where uname=@username and passwd=@password", sqlConnection);
+            cmd.Parameters.AddWithValue("@username", )
 
         }
     }
